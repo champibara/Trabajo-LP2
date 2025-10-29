52d2549e4143b9b61ee0c77970118c6912345a02
JhorLop134 <jhordy010304@gmail.com>
2025-10-28 20:30:41 -0500

    Agregar clase Inferencia

--- estadistica_lib/inferencia.py
+++ estadistica_lib/inferencia.py
@@ -5,5 +5,17 @@ import numpy as np
 from base import EstadisticaBase
 
 #Creación de clase
-
+class InferenciaEstadistica(EstadisticaBase):
+    """
+    Clase hija para realizar inferencia estadística avanzada (Pruebas de Hipótesis e ICs)
+    heredando los cálculos de la media y la varianza.
+    """
+    def __init__(self, datos):
+        """Inicializa la clase base."""
+        super().__init__(datos)
+        self._n = self.obtener_n_observaciones()
+        
+        if self._n < 2:
+             # Dejamos pasar para permitir pruebas con dos muestras
+             pass
 
